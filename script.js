e0ac8336b64796dd44959861479c55334a76482b
Rajeshkumar-7 <83906427+Rajeshkumar-7@users.noreply.github.com>
2024-03-26 16:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0;i<str.length;i++){
+    let s =  str.slice(0 , i) + str.slice(i+1)
+    if(!s.includes(str.slice(i , i+1))){
+        return str.slice(i , i+1)
+    }
+}
+return null
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
